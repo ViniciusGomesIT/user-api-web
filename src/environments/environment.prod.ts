301582e9769c44df5777394f74de05ef871eac95
vinicius.g.aguiar <vinicius.g.aguiar@accenture.com>
2019-10-14 13:46:01 -0300

    Ajustes heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://vini-user-service.herokuapp.com/',
-  auth: 'https://vini-auth-service.herokuapp.com/'
+  api: 'https://vini-user-service.herokuapp.com',
+  auth: 'https://vini-auth-service.herokuapp.com'
 };
